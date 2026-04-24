2a1d0b8a5c06f4e253f5e2106e6446b57a8e8220
Sameer <330.sameer@gmail.com>
2026-04-24 17:49:42 +0530

    feat: add async source resolver and update resolver pipeline tests to use sync resolution

--- lib/any_image.dart
+++ lib/any_image.dart
@@ -1,2 +1,5 @@
 export 'src/model/source_type.dart';
 export 'src/widget/any_image.dart';
+export 'src/resolver/async_source_resolver.dart';
+export 'src/resolver/mime_resolver.dart';
+export 'src/resolver/resolver_pipeline.dart';
